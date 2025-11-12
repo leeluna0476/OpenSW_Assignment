f74c0e53b4913edd2c1d75c8d3c9ba28f172069a
Seo Jin Lee <leeluna0476@inu.ac.kr>
2025-11-12 12:27:21 +0000

    add header

---
+++ week09_git/include/myops.h
@@ -0,0 +1,14 @@
+#ifndef MYOPS_H
+#define MYOPS_H
+
+double myadd(double a, double b);
+double mysub(double a, double b);
+double mymul(double a, double b);
+double mydiv(double a, double b);
+
+#include <math.h>
+
+int mymod(int a, int b);
+double mypow(double a, double b);
+
+#endif
